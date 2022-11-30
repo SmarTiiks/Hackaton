e26da995407bce43bcf11bf557800d7bdbd5f793
Smar'Tiks <69517014+SmarTiiks@users.noreply.github.com>
2022-11-30 09:28:12 +0100

    [FIX] bouton nouvelle goutte
    Le boutton de nouvelle goutte fonctionne normalement:
         en appuyant sur le bouton, une nouvelle goutte apparait à des coordonnées aléatoires, si les coordonnées sont prises, ne fait rien.

--- Nouveau projet de jeu/Scripts/Goutte.gd
+++ Nouveau projet de jeu/Scripts/Goutte.gd
@@ -1,6 +1,8 @@
 extends Sprite
 
 var _timer = null
+var rng = RandomNumberGenerator.new()
+
 
 func _ready():
 	pass
@@ -49,11 +51,28 @@ func move_haut():
 			yield(t, "timeout")
 			self.translate(Vector2(0, -1))
 
+func to_nine_multiple(i):
+	for j in 1000:
+		if j > 0:
+			if abs(i -(j * 9)) < 9:
+				return j * 9
+	return 0 
+
 
 func _on_New_pressed():
+	rng.randomize()
+	var rand_y = rng.randi_range(0, 9*(Global.hauteur_grille-1))
+	var rand_x = rng.randi_range(0, 9*(Global.longueur_grille-1))
+	var pos = Vector2(to_nine_multiple(rand_x) + 5, to_nine_multiple(rand_y) + 5)
 	var Rond = get_child(0)
+	var tmp
+	for i in get_child_count():
+		tmp = get_child(i)
+		if tmp.position == pos:
+			return
+		
 	var child = Rond.duplicate()
 	child.name = "ron"
 	add_child(child)
-	child.translate(Vector2(5, 15))
+	child.position = pos
 	
